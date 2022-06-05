a45c09d8a8806a5a1e565936a4ebad9a8b37cbe6
Sergiy Shevchuk <sirjjjjjjj@gmail.com>
2022-06-05 11:55:28 +0300

    hw-5

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import App from 'App';
+import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter basename="/goit-react-hw-05-movies/">
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
